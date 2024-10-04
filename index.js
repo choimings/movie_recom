5baacf3da4e511a6944cef6fafd78be66e63f81e
ming <choimg51984@gmail.com>
2024-10-04 12:03:46 +0900

    add user based recommand

--- index.js
+++ index.js
@@ -150,6 +150,42 @@ app.get('/item-based/:item', (req, res) => {
   }
 });
 
+app.post('/user-based', (req, res) => {
+  try {
+    const scriptPath = path.join(__dirname, 'recommender.py');
+
+    const inputRatingDict = req.body;
+    const result = spawn(pythonExePath, [scriptPath, 'user-based']);
+
+    let responseData = '';
+
+    // 파이썬 스크립트로 JSON 데이터를 전달
+    result.stdin.write(JSON.stringify(inputRatingDict));
+    result.stdin.end(); // 더이상 데이터가 없으면 전달 끝
+
+    result.stdout.on('data', function (data) {
+      responseData += data.toString();
+    });
+
+    result.on('close', (code) => {
+      if (code === 0) {
+        const jsonResponse = JSON.parse(responseData);
+        res.status(200).json(jsonResponse);
+      } else {
+        res
+          .status(500)
+          .json({ error: `Child process exited with code ${code}` });
+      }
+    });
+
+    result.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
